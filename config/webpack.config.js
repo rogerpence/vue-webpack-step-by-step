491ed99e5ebc7f6ef0798fbf682dd76b6651c528
Roger Pence <roger.pence@gmail.com>
2020-03-11 20:55:57 -0500

    add .js to resolve.extensions

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 const DO_NOT_USE_EVAL_IN_BUILD = 'false';
 
-module.exports = {    
+module.exports = {
     mode: 'development',
     entry: {
         main: './src/app.js',
-    },        
+    },
     output: {
         path: path.join(__dirname, '../dist'),
         filename: '[name].build.js'
-    },        
+    },
     devtool: DO_NOT_USE_EVAL_IN_BUILD,
     module: {
         rules: [{
@@ -24,15 +24,15 @@ module.exports = {
         alias: {
             components: path.resolve(__dirname, '../src/components'),
         },
-        extensions: ['.vue'],
+        extensions: ['.vue', '.js'],
     },
     plugins: [
-        new VueLoaderPlugin(),        
+        new VueLoaderPlugin(),
     ],
     devServer: {
         host: 'localhost',
         contentBase: "./dist",
         hot: true,
         port: 8080,
-    },   
+    },
 }
